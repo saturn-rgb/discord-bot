f60185f06b27f09eaa880169926710ec3a0e87f8
saturn-rgb <michaelowie2000@gmail.com>
2026-01-19 21:41:18 +0100

    Initialize Discord bot with basic setup

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-"C:\Users\OWIE PRESCOTT\Desktop\bot\main.py"
+import discord
+from discord.ext import commands
+import os
+
+TOKEN = os.getenv("DISCORD_TOKEN")
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user}")
+
+bot.run(TOKEN)
